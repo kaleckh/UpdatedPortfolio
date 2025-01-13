fa4476156bea5548dea002728ccede71a7d7efdb
Justin <alexanderrr416@gmail.com>
2025-01-13 01:22:45 -0500

    chore: apply changes for responsiveness

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -1,62 +1,75 @@
 import styled from "styled-components";
-import ThemeSelector from "./ThemeSelector";
 import FadeIn from "./FadeIn";
+import ThemeSelector from "./ThemeSelector";
 
 const MenuContainer = styled.aside`
   min-height: 100vh;
-  position: fixed;
+  position: absolute;
   background-color: #0c0c15;
-  display: flex;
   flex-direction: column;
-  justify-content: space-between;
+  gap: 6rem;
+  align-items: center;
   border-right: 1px solid rgba(255, 255, 255, 0.3);
   padding: 2rem 0;
+  z-index: 7;
+  position: fixed;
+  justify-content: space-between;
+
+  display: none;
+
+  @media screen and (min-width: 1024px) {
+    display: flex;
+  }
 `;
 
 const MenuList = styled.ul`
-  display: flex;
-  flex-direction: column;
   list-style: none;
-  gap: 5rem;
+  gap: 1rem;
   align-items: center;
+  flex-direction: column;
   transform: translateY(3rem);
+  gap: 5rem;
+  display: flex;
 `;
 
 const MenuItem = styled.li`
-  font-size: 1.7rem;
-  transform: rotate(270deg);
+  font-size: 3rem;
   cursor: pointer;
+  transform: rotate(270deg);
+  font-size: 1.7rem;
 
   & > a {
-    color: #ccc;
+    color: #fff;
     text-decoration: none;
     transition: color 0.15s;
   }
-
   & > a:hover {
-    color: #fff;
+    filter: drop-shadow(0 0 3px #ccc);
   }
 `;
+
 const SideMenu = () => {
   return (
-    <MenuContainer>
-      <FadeIn delay={0}>
-        <MenuList>
-          <MenuItem>
-            <a href="">Projects</a>
-          </MenuItem>
-          <MenuItem>
-            <a href="">Skills</a>
-          </MenuItem>
-          <MenuItem>
-            <a href="">Contact</a>
-          </MenuItem>
-        </MenuList>
-      </FadeIn>
-      <FadeIn delay={0}>
-        <ThemeSelector />
-      </FadeIn>
-    </MenuContainer>
+    <>
+      <MenuContainer>
+        <FadeIn>
+          <MenuList>
+            <MenuItem>
+              <a href="">Projects</a>
+            </MenuItem>
+            <MenuItem>
+              <a href="">Skills</a>
+            </MenuItem>
+            <MenuItem>
+              <a href="">Contact</a>
+            </MenuItem>
+          </MenuList>
+        </FadeIn>
+        <FadeIn>
+          <ThemeSelector />
+        </FadeIn>
+      </MenuContainer>
+    </>
   );
 };
 
