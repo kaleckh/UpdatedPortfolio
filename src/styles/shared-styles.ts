b40e877d8a1c416a30be4b141ef841b5c43c6410
Justin <alexanderrr416@gmail.com>
2025-01-13 23:55:49 -0500

    chore: adjust button padding

--- src/styles/shared-styles.ts
+++ src/styles/shared-styles.ts
@@ -3,7 +3,7 @@ import styled, { css } from "styled-components";
 const baseBtn = css`
   outline: 0;
   border-radius: 5px;
-  padding: 1rem 2.5rem;
+  padding: 1.3rem 2.5rem;
   font-weight: 600;
   display: flex;
   gap: 0.5rem;
@@ -15,14 +15,14 @@ const baseBtn = css`
   }
 `;
 
-const primaryBtn = css`
+export const primaryBtn = css`
   ${baseBtn}
   border: 0;
   color: #000;
   background-color: ${(props) => props.theme.primary};
 `;
 
-const outlineBtn = css`
+export const outlineBtn = css`
   ${baseBtn}
   border: 1px solid ${(props) => props.theme.primary};
   color: ${(props) => props.theme.primary};
