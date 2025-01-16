a3e3614b7a7dcb1a95c306873b6e0e3790595220
Justin <alexanderrr416@gmail.com>
2025-01-16 00:38:54 -0500

    chore: move animation components to separate folder

--- src/components/animations/AnimatedGrid.tsx
+++ src/components/animations/AnimatedGrid.tsx
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion";
 import styled from "styled-components";
-import { useTheme } from "../hooks/useTheme";
+import { useTheme } from "../../hooks/useTheme";
 
 const GridWrapper = styled.div`
   position: absolute;
