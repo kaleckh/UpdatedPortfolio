6cbed492ea9197cae4752f1bb446cdd037e06859
Justin <alexanderrr416@gmail.com>
2025-01-16 20:12:09 -0500

    chore: add animation to heading

--- src/components/SectionHeading.tsx
+++ src/components/SectionHeading.tsx
@@ -1,4 +1,6 @@
+import { motion } from "framer-motion";
 import styled from "styled-components";
+import AnimatedText from "./animations/AnimatedText";
 
 interface Props {
   heading: string;
@@ -15,23 +17,55 @@ const HeadingWrapper = styled.div`
   align-items: center;
 `;
 
-const Bullet = styled.span`
+const BulletWrapper = styled.div`
+  display: inline;
+`;
+
+const Bullet = styled(motion.div)`
   color: ${(props) => props.theme.primary};
+  display: inline-block;
+  transform-origin: bottom;
 `;
 
 const Separator = styled.hr`
-  border: 1px solid rgba(255, 255, 255, 0.1);
+  border: 1px solid rgba(255, 255, 255, 0.3);
   width: 100%;
   flex-grow: 1;
 `;
+
+const animationVariants = {
+  hidden: {
+    opacity: 0,
+    scale: 0,
+  },
+  visible: {
+    opacity: 1,
+    scale: 1,
+  },
+};
 
 const SectionHeading = ({ heading }: Props) => {
   return (
     <HeadingContainer>
       <HeadingWrapper>
         <h2>
-          {heading}
-          <Bullet>.</Bullet>
+          <AnimatedText text={heading}></AnimatedText>
+          <BulletWrapper>
+            <Bullet
+              variants={animationVariants}
+              initial={"hidden"}
+              animate={"hidden"}
+              whileInView={"visible"}
+              transition={{
+                delay: 0.6,
+                type: "spring",
+                bounce: 0.5,
+                visualDuration: 0.2,
+              }}
+            >
+              .
+            </Bullet>
+          </BulletWrapper>
         </h2>
       </HeadingWrapper>
       <Separator />
