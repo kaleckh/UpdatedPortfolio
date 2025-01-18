c02f73ff9bd834786b7b83b880d689ac1f0fa1b0
Justin <alexanderrr416@gmail.com>
2025-01-18 01:33:43 -0500

    feat: add skill section

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import SectionHeading from "./SectionHeading";
 import SocialLinks from "./SocialLinks";
 import FadeUp from "./animations/FadeUp";
+import TechSkills from "./skills/TechSkills";
 
 const AboutContainer = styled.section`
   max-width: 85rem;
@@ -11,12 +12,33 @@ const AboutContainer = styled.section`
 
 const IntroHighlight = styled.span`
   font-size: 2rem;
-  letter-spacing: 0.2rem;
   color: ${(props) => props.theme.primary};
 `;
 
 const SocialWrapper = styled.div`
-  margin-top: 2rem;
+  margin-top: 3rem;
+  display: flex;
+  justify-content: end;
+
+  @media screen and (min-width: 768px) {
+    justify-content: start;
+  }
+`;
+
+const AboutWrapper = styled.div`
+  display: flex;
+  gap: 2rem;
+  flex-direction: column;
+
+  @media screen and (min-width: 768px) {
+    flex-direction: row;
+  }
+`;
+
+const About = styled.div`
+  @media screen and (min-width: 768px) {
+    max-width: 50rem;
+  }
 `;
 
 const AboutSection = () => {
@@ -24,50 +46,46 @@ const AboutSection = () => {
     <>
       <AboutContainer>
         <SectionHeading heading="About" />
-        <FadeUp>
-          <p>
-            <IntroHighlight>Here is my story. </IntroHighlight>My journey into
-            coding started with curiosity and grew into a love for solving
-            problems and creating impactful solutions. Whether designing
-            responsive frontends or building scalable backends, I thrive on
-            turning ideas into reality.
-          </p>
-        </FadeUp>
-        <br />
-        <FadeUp>
-          <p>
-            By day, I’m focused on delivering impactful code and collaborating
-            with talented teams. By night, I love diving into personal projects
-            or experimenting with new tools and frameworks to stay ahead of the
-            curve.
-          </p>
-        </FadeUp>
-        <br />
-        <FadeUp>
-          <p>
-            When I’m not coding, you’ll find me gaming away or looking for my
-            next read. I’m always open to new opportunities, exciting
-            challenges, or simply connecting with like-minded developers. Let’s
-            build something amazing together!
-          </p>
-        </FadeUp>
-        <FadeUp>
-          <SocialWrapper>
-            <SocialLinks />
-          </SocialWrapper>
-        </FadeUp>
+        <AboutWrapper>
+          <About>
+            <FadeUp>
+              <p>
+                <IntroHighlight>Here is my story. </IntroHighlight>My journey
+                into coding started with curiosity and grew into a love for
+                solving problems and creating impactful solutions. Whether
+                designing responsive frontends or building scalable backends, I
+                thrive on turning ideas into reality.
+              </p>
+            </FadeUp>
+            <br />
+            <FadeUp>
+              <p>
+                By day, I’m focused on delivering impactful code and
+                collaborating with talented teams. By night, I love diving into
+                personal projects or experimenting with new tools and frameworks
+                to stay ahead of the curve.
+              </p>
+            </FadeUp>
+            <br />
+            <FadeUp>
+              <p>
+                When I’m not coding, you’ll find me gaming away or looking for
+                my next read. I’m always open to new opportunities, exciting
+                challenges, or simply connecting with like-minded developers.
+                Let’s build something amazing together!
+              </p>
+            </FadeUp>
+            <FadeUp>
+              <SocialWrapper>
+                <SocialLinks />
+              </SocialWrapper>
+            </FadeUp>
+          </About>
+          <FadeUp>
+            <TechSkills />
+          </FadeUp>
+        </AboutWrapper>
       </AboutContainer>
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
     </>
   );
 };
