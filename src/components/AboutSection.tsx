7160be4e3488c41d530fa06e8185e0569c70286d
Justin <alexanderrr416@gmail.com>
2025-01-18 22:37:53 -0500

    chore: remove unused styles

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -3,11 +3,10 @@ import SectionHeading from "./SectionHeading";
 import SocialLinks from "./SocialLinks";
 import FadeUp from "./animations/FadeUp";
 import TechSkills from "./skills/TechSkills";
+import { container } from "../styles/shared-styles";
 
 const AboutContainer = styled.section`
-  max-width: 85rem;
-  margin: 0 auto;
-  padding: 0 2.4rem;
+  ${container}
 `;
 
 const IntroHighlight = styled.span`
@@ -43,50 +42,48 @@ const About = styled.div`
 
 const AboutSection = () => {
   return (
-    <>
-      <AboutContainer>
-        <SectionHeading heading="About" />
-        <AboutWrapper>
-          <About>
-            <FadeUp>
-              <p>
-                <IntroHighlight>Here is my story. </IntroHighlight>My journey
-                into coding started with curiosity and grew into a love for
-                solving problems and creating impactful solutions. Whether
-                designing responsive frontends or building scalable backends, I
-                thrive on turning ideas into reality.
-              </p>
-            </FadeUp>
-            <br />
-            <FadeUp>
-              <p>
-                By day, I’m focused on delivering impactful code and
-                collaborating with talented teams. By night, I love diving into
-                personal projects or experimenting with new tools and frameworks
-                to stay ahead of the curve.
-              </p>
-            </FadeUp>
-            <br />
-            <FadeUp>
-              <p>
-                When I’m not coding, you’ll find me gaming away or looking for
-                my next read. I’m always open to new opportunities, exciting
-                challenges, or simply connecting with like-minded developers.
-                Let’s build something amazing together!
-              </p>
-            </FadeUp>
-            <FadeUp>
-              <SocialWrapper>
-                <SocialLinks />
-              </SocialWrapper>
-            </FadeUp>
-          </About>
+    <AboutContainer>
+      <SectionHeading heading="About" />
+      <AboutWrapper>
+        <About>
           <FadeUp>
-            <TechSkills />
+            <p>
+              <IntroHighlight>Here is my story. </IntroHighlight>My journey into
+              coding started with curiosity and grew into a love for solving
+              problems and creating impactful solutions. Whether designing
+              responsive frontends or building scalable backends, I thrive on
+              turning ideas into reality.
+            </p>
           </FadeUp>
-        </AboutWrapper>
-      </AboutContainer>
-    </>
+          <br />
+          <FadeUp>
+            <p>
+              By day, I’m focused on delivering impactful code and collaborating
+              with talented teams. By night, I love diving into personal
+              projects or experimenting with new tools and frameworks to stay
+              ahead of the curve.
+            </p>
+          </FadeUp>
+          <br />
+          <FadeUp>
+            <p>
+              When I’m not coding, you’ll find me gaming away or looking for my
+              next read. I’m always open to new opportunities, exciting
+              challenges, or simply connecting with like-minded developers.
+              Let’s build something amazing together!
+            </p>
+          </FadeUp>
+          <FadeUp>
+            <SocialWrapper>
+              <SocialLinks />
+            </SocialWrapper>
+          </FadeUp>
+        </About>
+        <FadeUp>
+          <TechSkills />
+        </FadeUp>
+      </AboutWrapper>
+    </AboutContainer>
   );
 };
 
