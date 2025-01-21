3a88f8a503f6a93fb01a12c5e15f456aa7234cdd
Justin <alexanderrr416@gmail.com>
2025-01-20 23:52:15 -0500

    chore: add reusable container style

--- src/styles/shared-styles.ts
+++ src/styles/shared-styles.ts
@@ -1,11 +1,21 @@
 import styled, { css } from "styled-components";
 
+export const container = css`
+  max-width: 85rem;
+  margin: 0 auto;
+  padding: 0 2.4rem;
+
+  @media screen and (min-width: 1150px) {
+    max-width: 95rem;
+  }
+`;
+
 const baseBtn = css`
   outline: 0;
   border-radius: 5px;
   padding: 1.3rem 2.5rem;
-  font-weight: 600;
   display: flex;
+  font-size: 1.6rem;
   gap: 0.5rem;
   align-items: center;
   transition: 0.15s ease-in;
