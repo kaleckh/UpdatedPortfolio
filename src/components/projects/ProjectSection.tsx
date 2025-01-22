4e7c56cea21eb109b8b4192b4ab118b53addc626
Justin <alexanderrr416@gmail.com>
2025-01-21 21:52:57 -0500

    feat: truncate project display

--- src/components/projects/ProjectSection.tsx
+++ src/components/projects/ProjectSection.tsx
@@ -1,8 +1,10 @@
+import { useState } from "react";
 import styled from "styled-components";
+import { projectData } from "../../data/projectData";
 import { container } from "../../styles/shared-styles";
 import SectionHeading from "../SectionHeading";
 import ProjectCard from "./ProjectCard";
-import { projectData } from "../../data/projectData";
+import { LuChevronsDown } from "react-icons/lu";
 
 const ProjectContainer = styled.section`
   ${container}
@@ -12,17 +14,59 @@ const ProjectContainer = styled.section`
 const Wrapper = styled.div`
   display: flex;
   gap: 2rem;
+  flex-wrap: wrap;
 `;
 
+const ToggleProjectButton = styled.button`
+  margin: 5rem auto;
+  font-size: 1.8rem;
+  border: 0;
+  outline: 0;
+  display: flex;
+  align-items: center;
+  letter-spacing: 0.4rem;
+  gap: 0.7rem;
+  background: 0;
+  color: ${(props) => props.theme.primary};
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+const ExpandIcon = styled(LuChevronsDown)`
+  font-size: 1.8rem;
+
+  &.rotate {
+    transform: rotate(180deg);
+  }
+`;
+
+// const truncatedProjectData = projectData.
+
 const ProjectSection = () => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const truncatedProjectData = isExpanded
+    ? projectData
+    : projectData.slice(0, 4);
+
+  const handleShowAllProjects = () => {
+    setIsExpanded(!isExpanded);
+  };
+
   return (
     <ProjectContainer>
       <SectionHeading heading="Projects" headingOrder={1} />
       <Wrapper>
-        {projectData.map((project) => (
-          <ProjectCard {...project} />
+        {truncatedProjectData.map((project, index) => (
+          <ProjectCard key={index} {...project} />
         ))}
       </Wrapper>
+      <ToggleProjectButton onClick={handleShowAllProjects}>
+        {isExpanded ? "Show Less" : "Show More"}
+        <ExpandIcon className={isExpanded ? "rotate" : ""} />
+      </ToggleProjectButton>
       <br />
       <br />
       <br />
